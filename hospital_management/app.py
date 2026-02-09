01ab5c6c9b9e648efe100035870394e4ac124d7c
Suvrodip Datta <suvrodipdatta@gmail.com>
2026-02-09 08:28:39 +0530

    Milestone-0 HMS-MAD-1

--- hospital_management/app.py
+++ hospital_management/app.py
@@ -1,8 +1,1 @@
-
-from flask_sqlalchemy import SQLAlchemy
-from flask import Flask
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hospital.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+#
